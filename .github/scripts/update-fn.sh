565c8f23e6d4c43305c751749f850393b8a33fe7
iamkishorekumar-git <iamkishorekumar1@gmail.com>
2023-12-27 07:23:17 +0530

    Updated the env to prod

--- .github/scripts/update-fn.sh
+++ .github/scripts/update-fn.sh
@@ -1,33 +1,30 @@
 #!/bin/bash
 
+source lambda-constants.sh
 
-# Access arguments passed to the script
-AWS_ECR_ACCOUNT_URL="$1"  # First argument
-AWS_ECR_REPO="$2"        # Second argument
-TAG="$3"                  # Third argument (likely from an environment variable)
-AWS_REGION="$4"          # Fourth argument (likely from an environment variable)
+array_name="${FUNCTION_GROUP}_functions"
 
+AWS_ECR_ACCOUNT_URL="$1"
+AWS_ECR_REPO="$2"
+TAG="$3"
+AWS_REGION="$4"
+FUNCTION_GROUP="$5"
 
-echo $AWS_ECR_ACCOUNT_URL
-echo $AWS_ECR_REPO
-echo $TAG
-echo $AWS_REGION
+echo $array_name
 
-functions=("kk-test-fn"
-          "kk-test-beta-fn"
-)
+if [[ -v "${array_name}" ]]; then
+  for function in "${!array_name[@]}"; do  # Corrected array expansion
+    aws lambda update-function-code \
+        --function-name "$function" \
+        --image-uri "${AWS_ECR_ACCOUNT_URL}/${AWS_ECR_REPO}:${TAG}" \
+        --region "${AWS_REGION}" > /dev/null
 
-for function in "${functions[@]}"; do
-  aws lambda update-function-code \
-      --function-name "$function" \
-      --image-uri "${AWS_ECR_ACCOUNT_URL}/${AWS_ECR_REPO}:${TAG}" \
-      --region "${AWS_REGION}" > /dev/null
-
-  if [[ $? -ne 0 ]]; then
-    echo "Error updating function: $function"
-  else
-    echo "Function updated successfully: $function"
-  fi
-done
-
-
+    if [[ $? -ne 0 ]]; then
+      echo "Error updating function: $function"
+    else
+      echo "Function updated successfully: $function"
+    fi
+  done
+else
+  echo "Error: Array ${array_name} does not exist."
+fi

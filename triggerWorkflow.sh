8e5082ced2d8bae1ba09ead0fe72a55201963f1c
iamkishorekumar-git <iamkishorekumar1@gmail.com>
2024-01-12 12:54:01 +0530

    Added script for triggerWorkflow

---
+++ triggerWorkflow.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PR_NUMBER=$(curl -L \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $GITHUB_TOKEN" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/$ORG/$REPO/actions/workflows/deploy.yml/dispatches \
+  -d '{"ref":"master"}')
+
+echo $PR_NUMBER

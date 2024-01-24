26f749fad87683f99e7f2a522de235bfa11be810
KK-Repos <iamkishorekumar1@gmail.com>
2024-01-25 04:50:03 +0530

    Updated test-report yml

--- printResult.js
+++ printResult.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-		const data = fs.readFileSync('slackReport/output.json', { encoding: 'utf8', flag: 'r' });
+		const data = fs.readFileSync('resultTest.js', { encoding: 'utf8', flag: 'r' });
 		const output = JSON.parse(data);
         print("[output]",output)
 

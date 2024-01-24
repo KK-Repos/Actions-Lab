8a27d5c7888a25b118e421b3a7ecd07af445037f
KK-Repos <iamkishorekumar1@gmail.com>
2024-01-25 04:55:36 +0530

    Updated test-report yml

--- printResult.js
+++ printResult.js
@@ -2,6 +2,6 @@ const fs = require("fs");
 
 console.log(__dirname)
 
-const data = fs.readFileSync('resultTest.js', { encoding: 'utf8', flag: 'r' });
+const data = fs.readFileSync('result/resultTest.js', { encoding: 'utf8', flag: 'r' });
 const output = JSON.parse(data);
 print("[output]",output)

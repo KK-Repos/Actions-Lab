3c4801ec8f311db42ac3334243debcf13cf09c0a
KK-Repos <iamkishorekumar1@gmail.com>
2024-01-25 05:16:26 +0530

    Misc changes

--- printResult.js
+++ printResult.js
@@ -1,7 +1,13 @@
-const fs = require("fs");
+const fs = require('fs');
 
-console.log(__dirname)
+const data = fs.readFileSync(__dirname + '/resultData.json', { encoding: 'utf8', flag: 'r' });
 
-const data = fs.readFileSync('result/resultData.js', { encoding: 'utf8', flag: 'r' });
-const output = JSON.parse(data);
-print("[output]",output)
+try {
+  const jsonData = JSON.parse(data);
+
+  jsonData.forEach(item => {
+    console.log(item.text);
+  });
+} catch (error) {
+  console.error('Error parsing JSON:', error);
+}

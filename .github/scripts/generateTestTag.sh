5174f4f2d8e4771674caf4d778aa11d2d3593eb3
Kishore Kumar <55937175+KK-Repos@users.noreply.github.com>
2024-03-14 06:56:42 +0530

    Checking tag

---
+++ .github/scripts/generateTestTag.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Get current date and time
+current_time=$(date +"%Y%m%d%H%M%S")
+
+BRANCH_NAME="$1"
+
+# Replace '/' and space with '-' in the branch name
+formatted_branch=$(echo "$BRANCH_NAME" | sed 's/[\/ ]/-/g')
+
+# Convert the branch name to lowercase
+formatted_branch_lower=$(echo "$formatted_branch" | tr '[:upper:]' '[:lower:]')
+
+# Append current time to the formatted and lowercase branch name
+final_branch_name="${formatted_branch_lower}-${current_time}"
+
+# Output the final branch name as key-value pair
+echo "::set-output name=branch_name::$final_branch_name"
